c023008d795bc2bc9a7ef78d2de4c328e366100b
Alex <alejperez@birt.eus>
2022-11-22 00:37:53 +0100

    Tarea revisada

--- src/app/pages/tabs-routing.module.ts
+++ src/app/pages/tabs-routing.module.ts
@@ -16,8 +16,8 @@ const routes: Routes = [
         loadChildren: () => import('./leer/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'info',
+        loadChildren: () => import('./info/tab3.module').then(m => m.Tab3PageModule)
       },
       {
         path: '',
